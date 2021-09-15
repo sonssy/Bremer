af285dd739731b4afcf7ffa4783259e1d07aa957
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-09-15 11:44:52 +0900

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,19 @@
+package data_operation;
+
+import javax.swing.JOptionPane;
+
+public class casting {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+        int b = (int) 1.1;
+        
+        String id =  JOptionPane.showInputDialog("Enter a ID");
+        
+        System.out.println(id);
+        
+        
+	}
+
+}
+  
